65aeb499060abd7e579be6832f6f48e17fce8322
Kontertj <128936128+Konterj@users.noreply.github.com>
2025-04-10 21:05:03 +0300

    Update MobileController.cs

--- Assets/Scripts/Controller/MobileController.cs
+++ Assets/Scripts/Controller/MobileController.cs
@@ -43,17 +43,6 @@ public class MobileController : MonoBehaviour
     {
         OnCalcueRotatedTouch();
         OnCalcueMovingFingerId();
-        /*switch(touchCouse)
-        {
-            case 0: 
-            break;
-            case 1: Debug.Log("Add speed Right Rot touchCose:" + touchCouse);
-                RotatedVeloc.AddTorque(touchCouse * RotationSpeed * Time.fixedDeltaTime);
-                break;
-            case -1: Debug.Log("Add speed Left Rot, touchCose: " + touchCouse);
-                RotatedVeloc.AddTorque(touchCouse * RotationSpeed * Time.fixedDeltaTime);
-                break;
-        }*/
         //Left side
         if (touchLeft == 1 && RotatedVeloc.rotation < 80)
         {
@@ -74,50 +63,6 @@ public class MobileController : MonoBehaviour
         foreach (Touch touch in Input.touches)
         {
             Vector2 touchPosition = touch.position;
-            /*switch (touch.phase) 
-            {
-                case TouchPhase.Began:
-                    if (RectTransformUtility.RectangleContainsScreenPoint(GetRectLeft, touchPosition) && LeftFingerId == -1)
-                    {
-                        LeftFingerId = touch.fingerId;
-                        touchLeft = 1;
-                        Debug.Log("Move left");
-                    }
-                    else if (RectTransformUtility.RectangleContainsScreenPoint(GetRectRight, touchPosition) && RightFingerId == -1) 
-                    {
-                        RightFingerId = touch.fingerId;
-                        touchRight = -1;
-                        Debug.Log("Move Right");
-                    }
-                        break;
-                case TouchPhase.Stationary:
-                case TouchPhase.Moved:
-                    if (RectTransformUtility.RectangleContainsScreenPoint(GetRectLeft, touchPosition) && LeftFingerId == -1)
-                    {
-                        LeftFingerId = touch.fingerId;
-                        touchLeft = 1;
-                        touchRight = 0;
-                        RightFingerId = -1;
-                    }
-                    else if (RectTransformUtility.RectangleContainsScreenPoint(GetRectRight, touchPosition) && RightFingerId == -1)
-                    {
-                        RightFingerId = touch.fingerId;
-                        touchRight = -1;
-                        touchLeft = 0;
-                        LeftFingerId = -1;
-                    }
-                break;
-
-
-                case TouchPhase.Ended:
-                case TouchPhase.Canceled:
-                    touchRight = 0;
-                    touchLeft = 0;
-                    if (touch.fingerId == LeftFingerId) { LeftFingerId = -1; Debug.Log("You leave finger in rect Left"); }
-                    else if (touch.fingerId == RightFingerId) { RightFingerId = -1; Debug.Log("You leave finger in rect Right"); }
-                break;
-            }
-            */
             //It begin
             if(touch.phase == TouchPhase.Began && RectTransformUtility.RectangleContainsScreenPoint(GetRectLeft, touchPosition))
             {
