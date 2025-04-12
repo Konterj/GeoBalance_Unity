18d1a5b2debbfcba6382f2940f629fa26a161ff0
Kontertj <128936128+Konterj@users.noreply.github.com>
2025-04-12 11:19:33 +0300

    Update scripts, and add managmentController

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -6,11 +6,13 @@ public class GameManager : MonoBehaviour
 {
     public SpawnFigures spawnFigures;
     public FigureSetting figureSetting;
+    public ManagmentController managmentController;
     // Start is called before the first frame update
     void Start()
     {
         spawnFigures.Setting = figureSetting;
         spawnFigures.OnStartSpawn();
+        managmentController.SetController();
     }
 
     // Update is called once per frame
--- Assets/Scripts/Core/ManagmentController.cs
+++ Assets/Scripts/Core/ManagmentController.cs
@@ -1,18 +1,39 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ManagmentController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public List<Controller> controllers;
 
-    // Update is called once per frame
-    void Update()
+
+    public void SetController()
     {
-        
+        bool IsMobile = Application.isMobilePlatform;
+        for (int i = 0; i < controllers.Count; i++) 
+        {
+            controllers[i].ControllerList.enabled = false;
+        }
+        if (IsMobile)
+        {
+            Debug.Log("Is mobile");
+            controllers[0].GetObject.SetActive(false);
+            controllers[1].Equals(true);
+            controllers[1].GetObject.SetActive(true);
+        }
+        else if(!IsMobile)
+        {
+            Debug.Log("Is not mobile");
+            controllers[0].Equals(true);
+            controllers[0].GetObject.SetActive(true);
+            controllers[1].GetObject.SetActive(false);
+        }
     }
+}
+
+[System.Serializable]
+public class Controller
+{
+    public string name;
+    public MonoBehaviour ControllerList;
+    public GameObject GetObject;
 }
